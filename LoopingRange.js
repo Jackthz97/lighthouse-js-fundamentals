87b296a7fc39a440f886820b41981cd46c5c71a3
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-01-18 17:50:08 -0500

    Passed Looping - Range Assignment

---
+++ LoopingRange.js
@@ -0,0 +1,15 @@
+function range(x,y,z){
+  let array = [];
+  if ((x !== undefined && y !== undefined && z !== undefined) && (x <= y) && (z !== 0 && z >= 0)){
+    for (let i = x; i <= y; i += z){
+      array.push(i);
+    }
+    return array;
+  }
+  else{
+    return array;
+  }
+}
+console.log(range(0, 8, 2));
+console.log(range(10, 30, 5));
+console.log(range(-9, 3, 3));
