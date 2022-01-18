e56ba8934fdb4f3ccc9510427bc2edffb5acddd3
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-01-17 20:47:14 -0500

    Passed Age Calculator Assignment

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(x, y, z){
+	z -= y;
+	return x + " is " + z + " years old.";
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
