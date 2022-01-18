13fd45b44dabb41ade05a710488286f168ab8325
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-01-17 20:35:20 -0500

    Completed X Marks The Perfect Shot Assignment

---
+++ xMarksThePerfectShot.js
@@ -0,0 +1,23 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']
+const finalPosition = function (moves) {
+  let x = 0;
+  let y = 0;
+  let array = [];
+  for (let postions of moves){
+    if (postions === "north"){
+      y++;
+    }
+    else if (postions === "south"){
+      y--;
+    }
+    else if (postions === "east"){
+      x++;
+    }
+    else if(postions === "west"){
+      x--;
+    }
+  }
+  array.push(x,y);
+  return array;
+}
+console.log(finalPosition(moves));
