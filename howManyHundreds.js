2546884fd0b5b7e8e9f288d0d51601d068070609
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-01-17 21:04:57 -0500

    Passed howManyHundreds Assignment

--- howManyHundreds.js
+++ howManyHundreds.js
@@ -1,12 +1,14 @@
 function howManyHundreds(x){
-	let y = x;
-	x %= 100;
-	y -= x;
-	y /= 100;
-	return y;
+  let y = x;
+  x %= 100;
+  y -= x;
+  y /= 100;
+  return y;
 }
 console.log(howManyHundreds(1000), "=?", 10);
 console.log(howManyHundreds(894), "=?", 8);
 console.log(howManyHundreds(520), "=?", 5);
 console.log(howManyHundreds(99), "=?", 0);
 console.log(howManyHundreds(0), "=?", 0);
+
+//Passed
